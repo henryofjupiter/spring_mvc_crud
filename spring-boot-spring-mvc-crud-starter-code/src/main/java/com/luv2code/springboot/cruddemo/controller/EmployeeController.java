01c183348d536ca31ddea1fb016b6a19b7b762e3
henryofjupiter <oshardhenry@gmail.com>
2024-09-05 23:46:23 -0400

    adds constructor for constroller injection, along along with GetMapping method, listEmployees, which will return a list of employee to the view page

--- spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -1,5 +1,37 @@
 package com.luv2code.springboot.cruddemo.controller;
 
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees") // "/employees is the base mapping url requests for items in this controller
 public class EmployeeController {
-    
+
+    private final EmployeeService employeeService;
+
+    // constructor for constructor injection, and inject the EmployeeService
+    // since we only have one constructor, @Autowiring is optional
+    public EmployeeController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
+
+
+    }
+    // add mapping for listing employees
+    @GetMapping("/list")
+    public String listEmployees(Model theModel){
+
+        // get employees from the database
+        List<Employee> employees = employeeService.findAll();
+
+        // add data to the spring model
+        theModel.addAttribute("employees", employees);
+
+        return "list-employees"; // name of the view page we return the list to
+    }
 }
