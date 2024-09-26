034ebbf0b2aa397c7cd8400dd1334bee29ac95c4
henryofjupiter <oshardhenry@gmail.com>
2024-09-26 17:34:53 -0400

    adds code to employee controller for saving a employee to the database

--- spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -44,5 +46,18 @@ public class EmployeeController {
         theModel.addAttribute("employee", theEmployee);
 
         return "employees/employee-form";
+    }
+
+    // add mapping for saving an employee
+    @PostMapping("/save")
+    // form data being passed in from the html data binding th:object="${employee}"
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+
+        // save the employee
+        employeeService.save(theEmployee);
+
+        // user a redirect to prevent duplicate submissions
+        return  "redirect:/employees/list";
+
     }
 }
